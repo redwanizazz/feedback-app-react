c9c016a13e1ac264196a8947ed464e0865ae8de5
Redwan <redwanizaz10@gmail.com>
2025-03-02 23:06:30 +0600

    Chapter 5 End

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
-import { v4 as uuidv4 } from 'uuid'
+import { v4 as uuidv4 } from 'uuid';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import { useState } from 'react';
 import Header from "./components/Header";
 import FeedbackList from "./components/FeedbackList";
 import FeedbackStats from './components/FeedbackStats';
 import FeedbackData from './data/feedbackData';
 import FeedbackForm from './components/FeedbackForm';
+import AboutLinkIcon from './components/AboutLinkIcon';
+import AboutPage from './pages/AboutPage';
 
 function App() {
     const [feedback, setFeedback] = useState(FeedbackData)
@@ -21,15 +24,24 @@ function App() {
     }
 
     return (
-        <>
+        <Router>
             <Header text="Feedback UI" bgColor="rgba(0, 0, 0, 0.4)" textColor="#ff6a95" />
             <div className="container">
-                <FeedbackForm handleAdd={addFeedback} />
-                <FeedbackStats feedback={feedback} />
-                <FeedbackList feedback={feedback} handleDelete={deleteFeedback} />
+                <Routes>
+                    <Route path="/" element={
+                        <>
+                            <FeedbackForm handleAdd={addFeedback} />
+                            <FeedbackStats feedback={feedback} />
+                            <FeedbackList feedback={feedback} handleDelete={deleteFeedback} />
+                        </>
+                    } />
+                    <Route path="/about" element={<AboutPage />} />
+                </Routes>
+
+                <AboutLinkIcon />
             </div>
-        </>
-    );
+        </Router>
+    )
 }
 
 export default App;
---
+++ src/pages/AboutPage.jsx
@@ -0,0 +1,27 @@
+import Card from "../components/shared/Card";
+import { Link } from "react-router-dom";
+
+function AboutPage() {
+  return (
+    <Card>
+      <div>
+        <div className="about">
+          <h1>My First React Project</h1>
+          <p>Features of this app: </p>
+          <ul>
+            <li style={{listStyleType: 'circle'}}>Collect user feedback along with ratings and append them to existing data.</li>
+            <li style={{listStyleType: 'circle'}}>Display the total number of reviews and their average rating.</li>
+            <li style={{listStyleType: 'circle'}}>Allow feedback deletion for specific purposes, but restore them upon server restart.</li>
+          </ul>
+          <p>Version: 1.0.0</p>
+
+          <p>
+            <Link to="/">Back to Home</Link>
+          </p>
+        </div>
+      </div>
+    </Card>
+  );
+}
+
+export default AboutPage;
